806075556b4e30b524fcc8c064c7e4c8ea225906
Admin <Admin@Medici>
2025-11-05 10:53:29 +0800

    Initial Commit.

--- Game Development/src/Entity/Entity.java
+++ Game Development/src/Entity/Entity.java
@@ -9,7 +9,9 @@ public class Entity {
 	public int speed;
 	
 	// Common properties for all entities (player, enemies, NPCs)
-	public BufferedImage up1, up2, down1, down2, left1, left2, right1, right2;
+	public BufferedImage up1, up2,up3,up4,up5,up6,up7,up8, down1, down2, down3, down4, down5,down6,down7,
+	down8, left1, left2,left3,left4,left5,left6,left7,left8, right1, right2,right3,
+	right4,right5,right6,right7,right8;
 	public String direction;// Current facing direction
 	
 	public int spriteCounter = 0;// Counts frames for switching sprites
--- Game Development/src/Entity/Player.java
+++ Game Development/src/Entity/Player.java
@@ -36,14 +36,41 @@ public class Player extends Entity{
 		
 		try {
 			//Load images here
-			up1 = ImageIO.read(getClass().getResourceAsStream("/player/boy_up_1.png"));
-			up2 = ImageIO.read(getClass().getResourceAsStream("/player/boy_up_2.png"));
-			down1 = ImageIO.read(getClass().getResourceAsStream("/player/boy_down_1.png"));
-			down2 = ImageIO.read(getClass().getResourceAsStream("/player/boy_down_2.png"));
-			left1 = ImageIO.read(getClass().getResourceAsStream("/player/boy_left_1.png"));
-			left2 = ImageIO.read(getClass().getResourceAsStream("/player/boy_left_2.png"));
-			right1 = ImageIO.read(getClass().getResourceAsStream("/player/boy_right_1.png"));
-			right2 = ImageIO.read(getClass().getResourceAsStream("/player/boy_right_2.png"));
+			down1 = ImageIO.read(getClass().getResourceAsStream("/player/1n.png"));
+			down2 = ImageIO.read(getClass().getResourceAsStream("/player/2n.png"));
+			down3 = ImageIO.read(getClass().getResourceAsStream("/player/3n.png"));
+			down4 = ImageIO.read(getClass().getResourceAsStream("/player/4n.png"));
+			down5 = ImageIO.read(getClass().getResourceAsStream("/player/5n.png"));
+			down6 = ImageIO.read(getClass().getResourceAsStream("/player/6n.png"));
+			down7 = ImageIO.read(getClass().getResourceAsStream("/player/7n.png"));
+			down8 = ImageIO.read(getClass().getResourceAsStream("/player/8n.png"));
+			
+			right1 = ImageIO.read(getClass().getResourceAsStream("/player/1ws.png"));
+			right2 = ImageIO.read(getClass().getResourceAsStream("/player/2ws.png"));
+			right3 = ImageIO.read(getClass().getResourceAsStream("/player/3ws.png"));
+			right4 = ImageIO.read(getClass().getResourceAsStream("/player/4ws.png"));
+			right5 = ImageIO.read(getClass().getResourceAsStream("/player/5ws.png"));
+			right6 = ImageIO.read(getClass().getResourceAsStream("/player/6ws.png"));
+			right7 = ImageIO.read(getClass().getResourceAsStream("/player/7ws.png"));
+			right8 = ImageIO.read(getClass().getResourceAsStream("/player/8ws.png"));
+			
+			up1 = ImageIO.read(getClass().getResourceAsStream("/player/1wb.png"));
+			up2 = ImageIO.read(getClass().getResourceAsStream("/player/2wb.png"));
+			up3 = ImageIO.read(getClass().getResourceAsStream("/player/3wb.png"));
+			up4 = ImageIO.read(getClass().getResourceAsStream("/player/4wb.png"));
+			up5 = ImageIO.read(getClass().getResourceAsStream("/player/5wb.png"));
+			up6 = ImageIO.read(getClass().getResourceAsStream("/player/6wb.png"));
+			up7 = ImageIO.read(getClass().getResourceAsStream("/player/7wb.png"));
+			up8 = ImageIO.read(getClass().getResourceAsStream("/player/8wb.png"));
+			
+			left1 = ImageIO.read(getClass().getResourceAsStream("/player/1wc.png"));
+			left2 = ImageIO.read(getClass().getResourceAsStream("/player/2wc.png"));
+			left3 = ImageIO.read(getClass().getResourceAsStream("/player/3wc.png"));
+			left4 = ImageIO.read(getClass().getResourceAsStream("/player/4wc.png"));
+			left5 = ImageIO.read(getClass().getResourceAsStream("/player/5wc.png"));
+			left6 = ImageIO.read(getClass().getResourceAsStream("/player/6wc.png"));
+			left7 = ImageIO.read(getClass().getResourceAsStream("/player/7wc.png"));
+			left8 = ImageIO.read(getClass().getResourceAsStream("/player/8wc.png"));
 			
 		}catch(IOException e) {
 			e.printStackTrace();
@@ -52,8 +79,8 @@ public class Player extends Entity{
 	
 	public void update() {
 		
-		//if(keyH.upPressed == true || keyH.downPressed == true || 
-		//keyH.leftPressed == true  || keyH.rightPressed == true) {
+		if(keyH.upPressed == true || keyH.downPressed == true || 
+		keyH.leftPressed == true  || keyH.rightPressed == true) {
 		
 			if(keyH.upPressed == true) {
 				direction = "up";
@@ -82,6 +109,7 @@ public class Player extends Entity{
 				}
 				spriteCounter = 0;
 			}
+		}
 		}	
 	//}
 	public void draw(Graphics g2) {
@@ -99,13 +127,50 @@ public class Player extends Entity{
 			if(spriteNum == 2) {
 				image = up2;
 			}
+			if(spriteNum == 3) {
+				image = up3;
+			}
+			if(spriteNum == 4) {
+				image = up4;
+			}
+			if(spriteNum == 5) {
+				image = up5;
+			}
+			if(spriteNum == 6) {
+				image = up6;
+			}
+			if(spriteNum == 7) {
+				image = up7;
+			}
+			if(spriteNum == 8) {
+				image = up8;
+			}
 			break;
+			
 		case "down":
 			if(spriteNum == 1) {
 				image = down1;
 			}
 			if(spriteNum == 2) {
 				image = down2;
+			}
+			if(spriteNum == 3) {
+				image = down3;
+			}
+			if(spriteNum == 4) {
+				image = down4;
+			}
+			if(spriteNum == 5) {
+				image = down5;
+			}
+			if(spriteNum == 6) {
+				image = down6;
+			}
+			if(spriteNum == 7) {
+				image = down7;
+			}
+			if(spriteNum == 8) {
+				image = down8; 
 			}
 			break;
 		case "left":
@@ -115,6 +180,24 @@ public class Player extends Entity{
 			if(spriteNum == 2) {
 				image = left2;
 			}
+			if(spriteNum == 3) {
+				image = left3;
+			}
+			if(spriteNum == 4) {
+				image = left4;
+			}
+			if(spriteNum == 5) {
+				image = left5;
+			}
+			if(spriteNum == 6) {
+				image = left6;
+			}
+			if(spriteNum == 7) {
+				image = left7;
+			}
+			if(spriteNum == 8) {
+				image = left8;
+			}
 			break;
 		case "right":
 			if(spriteNum == 1) {
@@ -123,9 +206,27 @@ public class Player extends Entity{
 			if(spriteNum == 2) {
 				image = right2;
 			}
+			if(spriteNum == 3) {
+				image = right3;
+			}
+			if(spriteNum == 4) {
+				image = right4;
+			}
+			if(spriteNum == 5) {
+				image = right5;
+			}
+			if(spriteNum == 6) {
+				image = right6;
+			}
+			if(spriteNum == 7) {
+				image = right7;
+			}
+			if(spriteNum == 8) {
+				image = right8;
+			}
 			break;
 		}
-		g2.drawImage(image, x, y, gp.tileSize, gp.tileSize, null);
+		g2.drawImage(image, x, y, gp.tileSize, gp.tileSize + 15, null);
 	}
 }
 
